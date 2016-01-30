78dc15f89b4f1f10ffe52a5c986e69c75f806726
Aurelien <aurelien.bauer@epitech.eu>
2016-01-30 17:33:23 +0100

    scène métro

---
+++ Assets/Scripts/Metro_move_background.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Metro_move_background : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
+        if (transform.position.x < -70.0f)
+        {
+         transform.position = new Vector3(transform.position.x + 70.0f, transform.position.y, transform.position.z);
+        }
+    }
+}
