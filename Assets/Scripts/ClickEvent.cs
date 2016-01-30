e95a4d850fa2bc31426c510d3b356cad325aa410
Franck MARCEL <Franck MARCEL>
2016-01-30 15:32:56 +0100

    Ajout de la chambre, des animations d evenements et de clics

---
+++ Assets/Scripts/ClickEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickEvent : MonoBehaviour
+{
+    public Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                anim.SetBool("clicked", true);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+            anim.SetBool("clicked", false);
+    }
+}
---
+++ Assets/Scripts/ParticleMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleMovement : MonoBehaviour
+{
+    public float particleMovement = 0.02f;
+    private float initialX;
+    private float initialY;
+
+    // Use this for initialization
+    void Start ()
+    {
+        initialX = transform.position.x;
+        initialY = transform.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Mathf.Abs(transform.position.x - initialX) > 0.2)
+            particleMovement = -particleMovement;
+        transform.position = new Vector3(transform.position.x + particleMovement, transform.position.y + particleMovement, transform.position.z);
+    }
+}
